24056b6d4e383ab2990e9ba6ce3ec0176588227e
LM12321 <logan.whitt@ku.edu>
2026-04-14 17:27:41 -0500

    added positions and scalarproducts

--- Assignment07.hs
+++ Assignment07.hs
@@ -46,5 +46,25 @@ find :: Eq a => a -> [(a, b)] -> [b]
     -if key == first element
         -add it to list
 -}
-
 find key list_of_tuples = [snd tuple| tuple <- list_of_tuples,  key == fst tuple]
+
+
+positions :: Eq a => a -> [a] -> [Int]
+{-
+    -create a list of tuples of item and index
+        -[(item0, index0), (item1, index1), ... ,(item_n, index_n)]
+    - use find with key and said list
+    - output will be answer
+-}
+
+positions key as = find key (zip as [0 .. length as])
+
+
+scalarproduct :: [Int] -> [Int] -> Int
+{-
+    - take in two lists
+    - get each pair out of the list (with the same index) using zip
+    - multiply their results together and put it into a new list
+    - sum that list up
+}
+scalarproduct as bs = sum [fst tuple * snd tuple | tuple <- zip as bs]
